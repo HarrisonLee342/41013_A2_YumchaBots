ad6a8e43cc3616a8e0dd778614eab182b1927145
Harrison <Harrison.s.lee@student.uts.edu.au>
2024-09-25 16:23:36 +1000

    GUI in MATLAB APP?

---
+++ GUI/GUI.m
@@ -0,0 +1,31 @@
+classdef GUI < handle
+%% USE MATLAB APPS GUI??
+    properties(Constant)
+        %% Constant variables
+
+    end
+
+    properties
+        %% Changing variables
+        
+        % Testing
+
+
+    end
+
+    methods
+        function self = GUI()
+
+        end
+    end
+
+    methods
+        function test(self)
+            
+        end
+
+        
+    end
+end
+
+
