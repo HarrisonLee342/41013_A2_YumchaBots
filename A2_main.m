919ca0c092b01c71e83e8e3bb7386119faf5e4a6
Harrison <Harrison.s.lee@student.uts.edu.au>
2024-09-30 18:39:55 +1000

    arm/object movement

--- A2_main.m
+++ A2_main.m
@@ -36,7 +36,8 @@ classdef A2_main < handle
 
                 switch input_val
                     case 't'
-                        self.test();
+                        self.testingCartMovement();
+                        % self.testingArmMovement();
 
                     case '0'
                         disp('Exiting...');
@@ -49,18 +50,18 @@ classdef A2_main < handle
     end
 
         methods
-            function test(self)
+            function testingArmMovement(self)
 
                 % % UR3 to plate Movement
                 % for i = 1:size(self.env.platesInitial,2)
                 %     platePose = self.env.platesInitial{i} * trotx(pi) * transl(0,0,-0.14);
-                %     self.move.objectMovement(platePose, self.env.ur3.model, self.steps);
+                %     self.move.armMove(platePose, self.env.ur3.model, self.steps);
                 % end
                 
                 % % KUKA to dumpling tray Movement
                 % for i= 1:size(self.env.dumplingsInitial,2)
-                %     dumplingPose = self.env.dumplingsInitial{4} * trotx(pi) * transl(0,0,-0.14);
-                %     self.move.objectMovement(dumplingPose, self.env.kuka.model, self.steps);
+                %     dumplingPose = self.env.dumplingsInitial{i} * trotx(pi) * transl(0,0,-0.14);
+                %     self.move.armMove(dumplingPose, self.env.kuka.model, self.steps);
                 % end
 
                 % % UR3 Range plot
@@ -71,33 +72,52 @@ classdef A2_main < handle
 
                 %% TESTING
 
-                % for i = 1:size(self.env.platesInitial,2)
-                %     platePose = self.env.platesInitial{2} * trotx(pi) * transl(0,0,-0.14);
-                %     self.move.objectMovement(platePose, self.env.ur3.model, self.steps);
+                % dumplingPose = self.env.dumplingsInitial{4} * trotx(pi) * transl(0,0,-0.14);
+                % self.move.armMove(dumplingPose, self.env.kuka.model, self.steps);
+                % 
+                % disp('end effector')
+                % disp(self.env.kuka.model.fkine(self.env.kuka.model.getpos).T)
+                % 
+                % for i = 1:size(self.env.dumplingsInitial,2)
+                %     fprintf('Dumpling no: %d \n',i);
+                %     disp(self.env.dumplingsInitial{i});
                 % end
+                % disp(size(self.env.dumplingsInitial));
 
-                dumplingPose = self.env.dumplingsInitial{4} * trotx(pi) * transl(0,0,-0.14);
-                self.move.objectMovement(dumplingPose, self.env.kuka.model, self.steps);
+                % Moving arm to plate, then moving plate to inside the cart
+                for i = 1:size(self.env.platesInitial,2)
+                    for j = 1:size(self.env.platesInitial,2)
+                        platePose = self.env.platesInitial{j} * trotx(pi) * transl(0,0,0.02);
+                        self.move.armMove(platePose, self.env.ur3.model, self.steps);
+                    end
 
-                disp('end effector')
-                disp(self.env.kuka.model.fkine(self.env.kuka.model.getpos).T)
-
-                for i = 1:size(self.env.dumplingsInitial,2)
-                    fprintf('Dumpling no: %d \n',i);
-                    disp(self.env.dumplingsInitial{i});
+                    for k = 1:size(self.env.platesInitial,2)
+                        platePose = self.env.carts.model{1}.base.T * transl(0,0,0.075) * trotx(pi);
+                        self.move.objectMove(platePose, self.env.ur3.model, self.steps, self.env.plates, 1);
+                    end
+                    self.env.plates.model{1}.base = self.env.carts.model{1}.base.T * transl(0,0,0.075);
+                    self.env.plates.model{1}.animate(0)
                 end
-                disp(size(self.env.dumplingsInitial));
+               
 
-                % for i = 1:size(self.env.platesInitial,2)
-                %     platePose = self.env.platesInitial(i,:).T * trotx(pi) * transl(0,0,-0.14);
-                %     self.move.objectMovement(platePose, self.env.kuka.model, self.steps);
-                % end
+            end
 
-                % for i = 1:size(self.env.platesInitial,2)
-                %     platePose = transl(self.env.platesInitial(i,:)) * trotx(pi) * transl(0,0,-0.14);
-                %     self.move.objectMovement(platePose, self.env.kuka.model, self.steps);
-                % end
+            function testingCartMovement(self)
+                
+                % self.move.cartMove(self.env.carts, 1, self.env.plates, 4, self.steps);
 
+                cartPose = self.env.carts.model{1}.base.T;
+
+                targetPose = transl(1,0,0);
+
+                q = jtraj(cartPose, targetPose, self.steps);
+
+                for j = 1:size(q,1)
+                    self.env.carts.model{1}.animate(q(j,:));
+                    drawnow();
+                    pause(0.01);
+                end
+                
             end
 
             % Sets variables with classes
